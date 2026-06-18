fa8c081bc2b03bdf12439ba1416e85a53e7c8dcd
Hoakyung <156088254+Hoakyung@users.noreply.github.com>
2026-06-18 18:11:18 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,46 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Console_Application_Assignment
+
+namespace EscapeSequenceChallenge
 {
-  class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Pic the number of your choice");
-            // Multiply by 50
-            //int num1 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(num1 * 50);
-
-            //Add 25
-            //int num2 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(num1 + 25);
-
-            //divid by 12.5
-            //float num3 = Convert.ToSingle(Console.ReadLine());
-            //Console.WriteLine(num3 / 12.5f);
-
-            //boolean check if greater than 50
-            //int num4 = Convert.ToInt32(Console.ReadLine());
-            //bool trueOrFalse = num4 > 50;
-            //Console.WriteLine(trueOrFalse.ToString());
-
-            //divide by 7 (modulus)
-            double num5 = Convert.ToDouble(Console.ReadLine());
-            double remainder = num5 % 7;
-            Console.WriteLine(remainder);
-
-
-
+            string doubleQuotes = "The use of double quotes\" \"is necessary";
+            Console.WriteLine(doubleQuotes);
+            Console.ReadLine();
+        
+        
         }
-
-
-
     }
-
-    
-    
 }
