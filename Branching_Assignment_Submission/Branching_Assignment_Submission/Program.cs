29a9c40c2796741d30bd10a6e99a0fd11171eab9
Hoakyung <156088254+Hoakyung@users.noreply.github.com>
2026-07-02 22:57:46 +0100

    Add files via upload

---
+++ Branching_Assignment_Submission/Branching_Assignment_Submission/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program will calculate the shipping cost of a package based on its weight and dimensions.
+            int weightLimit = 50;
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+            Console.WriteLine("Please enter the package weight: ");
+            int weightInput = Convert.ToInt32(Console.ReadLine());
+            // Initialize variables for width, height, and length
+            int widthInput = 0;
+            int heightInput = 0;
+            int lengthInput = 0;
+
+            // Check if the weight exceeds the limit
+            if (weightInput > weightLimit)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+                return;
+            }
+            // If the weight is within the limit, prompt the user for dimensions
+            else
+            {
+                Console.WriteLine("Please enter the package width: ");
+                widthInput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height: ");
+                heightInput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length: ");
+                lengthInput = Convert.ToInt32(Console.ReadLine());
+            }
+            // Calculate the total dimensions and check if it exceeds the limit
+            int totalDimensions = widthInput + heightInput + lengthInput;
+            if  (totalDimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+
+            // If the total dimensions are within the limit, calculate the shipping quote
+            else
+            {
+                int quote = (widthInput * heightInput * lengthInput * weightInput) / 100;
+                Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+                Console.WriteLine("Thank you!");
+            }
+            // Additional check for weight limit after dimensions input
+            if (weightInput > weightLimit)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+            }
+            // Recalculate total dimensions for the final check
+            totalDimensions = widthInput * heightInput * lengthInput;
+            // Final check for total dimensions
+            if (totalDimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            // Final calculation of shipping quote if all conditions are met
+            else if (totalDimensions <= 50)
+            {
+                int quote = (weightInput * widthInput * heightInput * lengthInput) / 100;
+                Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+                Console.WriteLine("Thank you!");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Branching_Assignment_Submission/Branching_Assignment_Submission/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Branching_Assignment_Submission")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Branching_Assignment_Submission")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4dc102b-338f-444d-bc6e-7f123f58eb9d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
